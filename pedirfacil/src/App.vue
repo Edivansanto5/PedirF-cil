772c2bef9eec6c2f675e4b0e65b5c22e22707478
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-29 11:53:37 -0300

    criando estrutura do projeto

---
+++ pedirfacil/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <pageHome/>
+  </div>
+</template>
+
+<script>
+
+import pageHome from './components/pageHome.vue'
+export default {
+  name: 'App',
+  components: {
+    pageHome
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pedirfacil/src/components/pageHome.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class=" container">
+        <div class="box">
+            <h1>Pedir-Facil</h1>
+            <hr>
+            <h2>Cardapio Digital</h2>
+        </div>
+        <div class="burgers">
+            <h3>Burgers</h3>
+        
+        </div>
+
+    </div>
+
+</template>
+
+<style>
+  h1,h2{
+    text-align: center;
+    color: #F8B502;
+  }
+  body{
+    background: #1B1B1B;
+  }
+  hr{
+    color: #F8B502;
+  }
+  .burgers{
+    height: 50px;
+    width: 400px;
+    text-align: center;
+    padding: 3px;
+    border-radius: 5px;
+    background: #F8B502;
+  }
+    
+</style>
