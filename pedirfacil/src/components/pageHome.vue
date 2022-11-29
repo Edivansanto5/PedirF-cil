1cde33d952d754d2d3b38d229140a69b00ac21cd
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-29 15:31:24 -0300

    criando novo componente

--- pedirfacil/src/components/pageHome.vue
+++ pedirfacil/src/components/pageHome.vue
@@ -1,37 +1,71 @@
 <template>
-    <div class=" container">
-        <div class="box">
-            <h1>Pedir-Facil</h1>
-            <hr>
-            <h2>Cardapio Digital</h2>
-        </div>
-        <div class="burgers">
-            <h3>Burgers</h3>
-        
-        </div>
+  <div class="container">
+      <h1 class="title">Pedir-Fácil</h1>
+      <div class="img">  
+          <img src="../assets/menu (1).jpg" alt=""> 
+      </div> 
+      <div class="btn">
+        <h2>Cardapio</h2>
+        <select>
+          <option>Oque Temos</option>
+          <option>Hamburger</option>
+          <option>Bolos</option>
+          <option>Salgados</option>
+          <option>Pizza</option>
+          <option>Bebidas</option>
 
-    </div>
+        </select>
 
+      </div>
+  </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
 <style>
-  h1,h2{
-    text-align: center;
-    color: #F8B502;
-  }
-  body{
-    background: #1B1B1B;
-  }
-  hr{
-    color: #F8B502;
-  }
-  .burgers{
-    height: 50px;
-    width: 400px;
-    text-align: center;
-    padding: 3px;
-    border-radius: 5px;
-    background: #F8B502;
-  }
-    
+body{
+  background: #1B1B1B;
+}
+.title{
+  color: #F8B502;
+  text-align: center;
+  font-size: 54px;
+}
+.img{
+  height: 200px;
+  
+}
+.btn{
+  background: red;
+  width: 100px;
+  margin: auto;
+ 
+}
+
+select{
+  font-size: 22px;
+  border-radius: 5px;
+ 
+}
+/* #inner {
+  margin: 0 auto;
+  width: 50%; Altere para o valor da largura desejada. 
+  background: #F8B502;
+  color: #F8B502;
+
+} */
+img{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    position: static;
+   
+    }
+    h2{
+      font-size: 40px;
+    }
 </style>
